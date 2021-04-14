a628189869b24eb47081b6efc83d07e6af96b73c
Rahul Pratap <rahulpratap85@gmail.com>
2021-04-14 18:38:44 +0100

    initial commit

---
+++ src/and.api/Controllers/ChatController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using ANDChatApp.Hubs;
+using ANDChatApp.ReqDto;
+
+namespace ANDChatApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/chat")]
+    [ApiController]
+    [ProducesResponseType(typeof(IEnumerable), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    public class ChatController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub> _hubContext;
+
+        public ChatController(IHubContext<ChatHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [Route("send")]                                           //path looks like this: https://localhost:44379/api/chat/send
+        [HttpPost]
+        public IActionResult SendRequest([FromBody] MessageDto msg)
+        {
+            _hubContext.Clients.All.SendAsync("ReceiveOne", msg.user, msg.msgText);
+            return Ok();
+        }
+
+        [Route("Foo")]                                           //path looks like this: https://localhost:44379/api/chat/send
+        [HttpGet]
+        public IActionResult GetRequest()
+        {            
+            return Ok("Ok from get ");
+        }
+
+    }
+}
---
+++ src/and.api/ReqDto/MessageDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ANDChatApp.ReqDto
+{
+    public class MessageDto
+    {
+        public string user { get; set; }
+        public string msgText { get; set; }
+
+
+    }
+}
